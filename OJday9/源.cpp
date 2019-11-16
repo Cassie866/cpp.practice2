e2e84ff3149e5d3e5456afb20fcfb052f9d7a811
Cassie <Cassie866@163.com>
2019-11-16 17:19:47 +0800

    动态规划

--- OJday9/源.cpp
+++ OJday9/源.cpp
@@ -2,32 +2,29 @@
 #include <vector>
 using namespace std;
 
-
 int fun(int m, int n)
 {
-	if (m < 1 || n < 1) 
-	{ 
-		return 0; 
-	}
-	vector<vector<int> > ret(m, vector<int>(n, 1));
-	for (int i = 1; i < m; ++i) 
+	if (m < 1 || n < 1)
 	{
-		for (int j = 1; j < n; ++j) 
-		{               
-			ret[i][j] = ret[i - 1][j] + ret[i][j - 1];
+		return 0;
+	}
+	vector<vector<int> > ret(m + 1, vector<int>(n + 1, 1));
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			ret[i + 1][j + 1] = ret[i][j + 1] + ret[i + 1][j];
 		}
 	}
-	return ret[m - 1][n - 1];
+	return ret[m][n];
 }
-
-
-
-
 
 int main()
 {
 	int n, m;
-	cin >> n >> m;
-	cout << fun(n, m);
+	while (cin >> n >> m)
+	{
+		cout << fun(n, m) << endl;
+	}
 	return 0;
 }
