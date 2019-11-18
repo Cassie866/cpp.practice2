60804303f16a028dd127a42f6b652d9b2539a561
Cassie <Cassie866@163.com>
2019-11-18 19:13:32 +0800

    另类加法，路径总数

--- OJday9/源.cpp
+++ OJday9/源.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -28,3 +28,29 @@ int main()
 	}
 	return 0;
 }
+
+
+//递归
+#include<iostream> 
+using namespace std; 
+int pathNum(int n,int m) 
+{ 
+	if(n > 1 && m > 1)
+		//b情况，递归   
+		return pathNum(n-1,m) + pathNum(n,m-1); 
+	else if(((n >= 1)&&(m == 1))||((n == 1)&&(m >= 1)))
+		// a情况，终止条件   
+		return n + m; 
+	else
+		//格子为0时， 路径为0  
+		return 0; 
+} 
+
+int main() 
+{ 
+	int n,m; 
+	while(cin>>n>>m) 
+	{
+		cout<<pathNum(n,m)<<endl; 
+	} return 0; 
+} 
