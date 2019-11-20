fbe79672cb982b5673d639acb7b3cb190ad19824
Cassie <Cassie866@163.com>
2019-11-20 11:55:36 +0800

    参数解析，跳石板（动规）

--- OJday13/源1.cpp
+++ OJday13/源1.cpp
@@ -33,3 +33,40 @@ int main()
 	}
 	return 0;
 }
+/*40%*/
+
+
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int N, M;
+	while (cin >> N >> M)
+	{
+		vector<int> v(M + 1, INT_MAX);
+		v[N] = 0;
+		for (int i = N; i <= M; i++)
+		{
+			if (v[i] == INT_MAX)
+				continue;
+			for (int j = 2; (j * j) <= i; j++)
+			{
+				if (i % j == 0)
+				{
+					if (i + j <= M)
+						v[i + j] = min(v[i] + 1, v[i + j]);
+					if (i + (i / j) <= M)
+						v[i + (i / j)] = min(v[i] + 1, v[i + (i / j)]);
+				}
+			}
+		}
+		if (v[M] == INT_MAX)
+			v[M] = -1;
+		cout << v[M] << endl;
+	}
+	return 0;
+}
