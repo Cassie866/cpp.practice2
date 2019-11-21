aac07002ca846599f2deabe3e7d5a3ec10fc458d
Cassie <Cassie866@163.com>
2019-11-21 11:27:59 +0800

    计算日期，幸运袋子

---
+++ OJday14/源.cpp
@@ -0,0 +1,34 @@
+/*计算日期是一年中的第几天*/
+
+#include <iostream>
+using namespace std;
+
+int getDays(int year, int month)
+{
+
+	int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	int day = days[month - 1];
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	{
+		if (month == 2)
+			day += 1;
+	}
+	return day;
+
+}
+
+int main()
+{
+	int year, month, day, days;
+	while (cin >> year >> month >> day)
+	{
+		days = 0;
+		for (int i = 1; i < month; i++)
+		{
+			days += getDays(year, i);
+		}
+		days += day;
+		cout << days << endl;
+	}
+	return 0;
+}
---
+++ OJday14/源1.cpp
@@ -0,0 +1,91 @@
+/*авдЫДќзг*/
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int sum(vector<int> v)
+{
+	int sum = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		sum += v[i];	
+	}
+	return sum;
+}
+
+int mut(vector<int> v)
+{
+	int mut = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		mut *= v[i];
+	}
+	return mut;
+}
+
+
+int main()
+{
+	int n = 0, kinds = 0;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		int temp = v[i];
+		v[i] = 1;
+		if (sum(v) - 1 > mutilation(v))
+		{
+			kinds++;
+		}
+		v[i] = temp;
+	}
+	cout << kinds;
+	return 0;
+}
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int sum(vector<int> v)
+{
+	int sum = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		sum += v[i];
+	}
+	return sum;
+}
+
+int mut(vector<int> v)
+{
+	int mut = 0;
+	for (int i = 0; i < v.size(); i++)
+	{
+		mut *= v[i];
+	}
+	return mut;
+}
+
+int main()
+{
+	int n = 0, kinds = 0;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i];
+	}
+
+
+	cout << kinds;
+	return 0;
+}
