6fe0c7eaaf8e24ad12151a0b9ef4188902897db9
Cassie <Cassie866@163.com>
2019-12-03 11:24:32 +0800

    洗牌，MP3光标位置

--- OJday21/源1.cpp
+++ OJday21/源1.cpp
@@ -1,33 +1,65 @@
-#include <iostream>
-using namespace std;
+#include  <iostream>
+#include  <string> 
+using namespace std; 
 int main()
-{
-	int n = 0;
-	string s;
-	while (cin >> n >> s)
-	{
-		int num1 = 0, num2 = 0;
-		if (n <= 4)
+{ 
+	//歌曲数量 
+	int n; 
+	//命令
+	string order; 
+	while (cin >> n >> order) 
+	{ //将n首歌曲编号1：n , num为光标当前所在歌曲的编号,first为当前屏幕显示页的第一首歌曲的编号 
+		int num = 1, first = 1; 
+		if (n <= 4) //歌曲总数不超过4时，所有歌曲一页即可显示完，不需翻页，first始终不变 
 		{
-			cout << 1 << 2 << 3 << 4 << endl;
+			for (int i = 0; i < order.size(); i++)
+			{
+				if (num == 1 && order[i] == 'U')
+					num = n;
+				else if (num == n && order[i] == 'D')
+					num = 1;
+				else if (order[i] == 'U')
+					num--;
+				else
+					num++;
+			}
+			for (int i = 1; i <= n - 1; i++)
+				cout << i << ' ';
+			cout << n << endl;
+			cout << num << endl;
 		}
-		char* p = &s[0];
-		for (int i = 0; i < s.length(); i++)
+		else //歌曲总数大于4时，显示完全所有歌曲需要翻页，屏幕总是显示4首歌曲 
 		{
-			if (s[i] == 'U')
-			{
-				if (p == &s[s.length() - 1])
-					p = &s[0];
-				p++;
+			for (int i = 0; i < order.size(); i++) 
+			{ 
+				if (first == 1 && num == 1 && order[i] == 'U') 
+				{ 
+					first = n - 3; num = n; 
+				} //特殊翻页1 
+				else if (first == n - 3 && num == n && order[i] == 'D') 
+				{ 
+					first = 1; 
+					num = 1; 
+				} //特殊翻页2 
+				else if (first != 1 && num == first && order[i] == 'U') 
+				{ 
+					first--; 
+					num--; 
+				} //一般翻页1 
+				else if (first != n - 3 && num == first + 3 && order[i] == 'D') 
+				{ 
+					first++; num++; 
+				} //一般翻页2 
+				else if (order[i] == 'U') 
+					num--; //其他情况1 
+				else
+					num++; //其他情况2 
 			}
-			if (s[i] == 'D')
-			{
-				if (p == &s[0])
-					p = &s[s.length() - 1];
-				p--;
-			}
-		}
-		cout << *p << endl;
+			for (int i = first; i < first + 3; i++) 
+				cout << i << ' '; 
+			cout << first + 3 << endl; 
+			cout << num << endl; 
+		} 
 	}
-		return 0;
+	return 0; 
 }
